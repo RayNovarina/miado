cda72dc2df193ddd10cb9e41b13f6e1bad08dc01
Ray <RNova94037@Gmail.com>
2016-05-12 16:22:54 -0700

    [WIP]taskbot-and-dm-channels: debugging expired token

--- app/models/concerns/channel_extensions.rb
+++ app/models/concerns/channel_extensions.rb
@@ -71,6 +71,8 @@ module ChannelExtensions
       @view ||= view
       @view.web_client ||= make_web_client
       # response is an array of hashes. Each has name and id of a team channel.
+      require 'pry'
+      binding.pry
       @view.web_client.channels_list['channels']
     end
 
